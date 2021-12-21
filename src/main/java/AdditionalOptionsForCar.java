00f87418dafd44b50c8bbdbeed9b35abfd7d7d27
VolodymyrDeveloper <xalimonenko@gmail.com>
2021-12-21 22:58:59 +0200

    first commit

---
+++ src/main/java/AdditionalOptionsForCar.java
@@ -0,0 +1,13 @@
+
+public class AdditionalOptionsForCar {
+
+    int comfortableSeats;
+
+    public int getComfortableSeats() {
+        return comfortableSeats;
+    }
+
+    public void setComfortableSeats(int comfortableSeats) {
+        this.comfortableSeats = comfortableSeats;
+    }
+}
